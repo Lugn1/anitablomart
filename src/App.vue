e482c4a5e15f5f8ac88208e8a1dfcd8ee5d5834d
William Karlström <wkarlstrom@outlook.com>
2024-01-04 01:40:43 +0100

    Remove old CSS and initiate header component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <script setup>
+import TheHeader from "@/components/TheHeader.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <TheHeader />
 
-    <div class="wrapper">
-      <p>"You did it!" </p>
-    </div>
-  </header>
-
-  <main>
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+body, html {
+  background-color: rgb(25, 25 ,25);
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 20%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background-color: olive;
+}
+
+
+.container img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
