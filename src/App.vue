378ff876269736a685436438383a06d4ee6de19b
William Karlström <wkarlstrom@outlook.com>
2024-01-04 01:03:58 +0100

    Remove template code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
@@ -8,12 +6,11 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <p>"You did it!" </p>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
