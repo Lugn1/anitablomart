0a1e6b5c6917efd34ee3c72b5c7bd7a227607210
William Karlström <wkarlstrom@outlook.com>
2024-01-04 18:18:42 +0100

    Add paths to router

--- src/main.js
+++ src/main.js
@@ -3,12 +3,22 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from "vue-router";
-import HomeIndex from "@/views/Home.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Contact from "@/views/Contact.vue";
+import Order from "@/views/Order.vue";
+import Blog from "@/views/Blog.vue";
+import Gallery from "@/views/Gallery.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', name: 'Home', component: HomeIndex}
+        {path: '/', name: 'Home', component: Home},
+        {path: '/about', name: 'About', component: About},
+        {path: '/blog', name: 'Blog', component: Blog},
+        {path: '/contact', name: 'Contact', component: Contact},
+        {path: '/gallery', name: 'Gallery', component: Gallery},
+        {path: '/order', name: 'Order', component: Order}
     ]
 })
 
