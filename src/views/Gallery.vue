562bcc74ec228bd967a24813ad480817620d7962
William Karlström <wkarlstrom@outlook.com>
2024-01-05 03:40:34 +0100

    Add some images in gallery and style

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,11 +3,77 @@
 </script>
 
 <template>
-  <div>
-    <h1>GALLERY</h1>
+  <div class="main-container">
+    <h1>Blåmesar</h1>
+    <div class="artworks">
+      <div class="top-row">
+        <div class="art-item">
+          <img src="/src/assets/images/egentid.png" alt="A lone bird">
+          <h3>Egentid</h3>
+        </div>
+        <div class="art-item">
+          <img src="/src/assets/images/komsaflygervi.png" alt="Two birds playing">
+          <h3>Kom så flyger vi</h3>
+        </div>
+      </div>
+      <div class="bottom-row">
+        <div class="art-item">
+          <img src="/src/assets/images/skavileka.png" alt="Bird asking the other to play">
+          <h3>Ska vi leka</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.artworks {
+  width: 40%;
+}
+
+.top-row, .bottom-row {
+  display: flex;
+  justify-content: center;
+}
+
+.art-item {
+  margin: 5px;
+  text-align: center;
+}
+
+
+.top-row .art-item {
+  width: 50%;
+}
+
+.top-row .art-item img {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 1 / 1;
+}
+
+
+.bottom-row .art-item {
+  width: 100%;
+}
+
+.bottom-row .art-item img {
+  width: 100%;
+  height: auto;
+
+}
+
+.artworks h3 {
+  margin-top: 5px;
+  font-style: italic;
+}
+
 
 </style>
