df8dfd08ce2490a7d964d8e28d5ba63f056a6e8f
William Karlström <wkarlstrom@outlook.com>
2024-01-11 14:05:10 +0100

    Change server.js to .cjs

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import serveStatic from 'serve-static';
-import path from 'path';
+import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -10,12 +10,14 @@ const __dirname = dirname(__filename);
 const app = express();
 
 // Serve the static files from the Vue app
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use('/', serveStatic(join(__dirname, 'dist')));
 
 // Handle SPA
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
