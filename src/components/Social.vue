8df0d7b5c7edd7ee5f6866e2fe970efe6c8ce44c
Stefan Karlsson <phtefan.karlsson@gmail.com>
2024-01-16 12:27:26 +0100

    Add Flag icons and Instagram link and icon.
    Co-authored-by: ArmanAlemirad <112554095+armanalemirad@users.noreply.github.com>
    Co-authored-by: William Carlström <112405777+lugn1@users.noreply.github.com>

---
+++ src/components/Social.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="SocialContainer">
+    <img src="../assets/icons/Instagram_Glyph_Gradient.png" alt="instagram icon">
+      <a href="https://www.instagram.com/ani_blom" target="_blank" rel="noopener noreferrer">
+        <button class="InstaButton">Min Instagram</button>
+      </a>
+  </div>
+</template>
+
+
+<style scoped>
+.SocialContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 20%;
+  height: 25%;
+  position: absolute;
+  top: 25%;
+  right: 0%;
+  background-color: transparent;
+  border-bottom: 2px solid rgba(30, 140, 60, 0.5%);
+}
+
+.SocialContainer img {
+  max-width: 40%;
+  max-height: 40%;
+  height: auto;
+  object-fit: cover;
+  object-position: bottom;
+
+
+}
+.InstaButton {
+
+  background: linear-gradient(to right, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);
+  color: white;
+  font-family: "Arsenal", sans-serif;
+  padding: 10px 20px;
+  margin-top: 5px;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+  outline: none;
+  transition: background 0.3s ease;
+}
+
+.InstaButton:hover {
+  background: linear-gradient(to right, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);
+  filter: brightness(1.2);
+}
+
+</style>
