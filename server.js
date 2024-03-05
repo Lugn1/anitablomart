7bcb6b31981b37060370264fc7c411895ebf5426
William Karlström <wkarlstrom@outlook.com>
2024-03-05 21:06:13 +0100

    Modify server.js

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const path = require('path');
 const history = require('connect-history-api-fallback');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(history({
     disableDotRule: true,
     verbose: true
 }));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 80; // You can choose any port
+const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
